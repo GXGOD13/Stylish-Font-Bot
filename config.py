d90fec8debb5784ec4d7cdd3a3b2294e9a2e3097
GXGOD13 <gxgod13@gmail.com>
2025-05-10 09:48:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ ADMIN = int(os.environ.get("ADMIN", "7562079827"))
 
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002580205471"))
 
-DB_URI = os.environ.get("DB_URI", "")
-DB_NAME = os.environ.get("DB_NAME", "gxmon239")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://gxmon239:f4l7bKrhka3Fh2cV@cluster0.qmblwql.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "")
 
 IS_FSUB = os.environ.get("IS_FSUB", "False").lower() == "true"  # Set "True" For Enable Force Subscribe
 AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-1002553401644").split()))  # Add Multiple channel id's
